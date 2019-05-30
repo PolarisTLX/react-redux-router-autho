3e82d651d5307359d70dd1e2e29c8410d2a469bd
Paul Rail <paul.a.rail@gmail.com>
2019-05-30 19:45:20 -0400

    install redux / react-redux + wrap <App/> with <Provider>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducers/reducer1';
+import { createStore } from 'redux';
+
+let store = createStore(rootReducer)
+
+// Wrapping the <App/> in <Provider>...</Provider> 
+// makes the Redux store / and Redux State available to the entire app
+ReactDOM.render(
+  <Provider store={store}>,
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 
