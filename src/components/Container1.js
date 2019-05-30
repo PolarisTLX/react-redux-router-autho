fc421a5d6394a8ba83b2b877b69766d853b7bc40
Paul Rail <paul.a.rail@gmail.com>
2019-05-30 19:51:07 -0400

    mapStateToProps(state) and mapDispatchToProps(dispatch)

---
+++ src/components/Container1.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import 
+
+// a simple container to test out Redux State
+class Container1 extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+
+      </div>
+    );
+  }
+}
+ 
+export default Container1;
