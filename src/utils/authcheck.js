d9eae25a17029f67f031c6e961093cfd492a6e96
Paul Rail <paul.a.rail@gmail.com>
2019-05-31 14:08:30 -0400

    setting up Auth0 with the Redux portion

---
+++ src/utils/authcheck.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AuthCheck extends Component {
+
+  render() { 
+    return ( 
+      <div>
+
+      </div>
+     );
+  }
+}
+ 
+export default AuthCheck
+<div>
+
+</div>;
