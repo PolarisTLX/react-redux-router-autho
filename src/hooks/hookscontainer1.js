8cb8b75804e1db35f71a6d02e467c35b15ee901f
Paul Rail <paul.a.rail@gmail.com>
2019-06-03 00:03:47 -0400

    implemented useEffect Hook

--- src/hooks/hookscontainer1.js
+++ src/hooks/hookscontainer1.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const HooksContainer1 = () => {
 
@@ -7,11 +7,22 @@ const HooksContainer1 = () => {
   //  OR :
   const [stateValue, setValue] = useState(0)
 
+  const [useEffectValue, setUseEffectValue] = useState(null)
+
+  useEffect(() => {
+    // simulate API request to get data from a server:
+    setTimeout(() => setUseEffectValue("this shows up after 3s if useEffect Worked"), 3000);
+  }, [stateValue])
+
   const incrementHookValue = () => {
     setValue(stateValue + 1)
   }
   const decrementHookValue = () => {
     setValue(stateValue - 1)
+  }
+
+  const changeuseEffectValue = () => {
+    setUseEffectValue('We changed the useEffectValue!')
   }
 
   return ( 
@@ -21,6 +32,12 @@ const HooksContainer1 = () => {
       <button onClick={ () => incrementHookValue()}>Increment with Hooks</button>
       <button onClick={() => decrementHookValue()}>Decrement with Hooks</button>
       <p>Hooks Local state value: {stateValue}</p>
+      <br/>
+      <button onClick={() => changeuseEffectValue()}>change useEffect Value</button>
+      {useEffectValue
+        ? <p>{useEffectValue}</p>
+        : <p>No Value</p>
+      }
     </div>
    );
 }
